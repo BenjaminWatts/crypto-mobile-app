5526f81d770b09357c17eb9bd941bce7e985b9a0
ben@kilowatts.io <ben@kilowatts.io>
2023-12-12 22:36:03 +0000

    start new market list

--- atoms/lists.tsx
+++ atoms/lists.tsx
@@ -30,3 +30,66 @@ export const FavouriteCoin: React.FC<FavouriteCoinProps> = ({
     </ListItem>
   );
 };
+
+type LiveMarketCoinListProps = {
+  name: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+};
+
+const formatters = {
+  priceUsd: (dollars: number) => {
+    if (dollars < 1) {
+      // return as cents
+      const cents = Math.round(dollars * 100);
+      return `${cents}¢`;
+    }
+    if (dollars < 100) {
+      const rounded = Math.round(dollars * 100) / 100;
+      return `$${rounded}`;
+    }
+    const rounded = Math.round(dollars);
+    return `$${rounded}`;
+  },
+  percentage: (percentage: number) => {
+    return `${Math.round(percentage * 100)}%`;
+  },
+};
+
+export const LiveMarketCoinList: React.FC<LiveMarketCoinListProps> = ({
+  name,
+  image,
+  current_price,
+  price_change_percentage_24h,
+}) => {
+  return (
+    <ListItem>
+      <ListItem.Content>
+        <View
+          style={{
+            display: "flex",
+            width: "100%",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ width: "50%" }}>
+            <ListItem.Title>{name}</ListItem.Title>
+          </View>
+          <View style={{ width: "20%", alignItems: 'flex-end' }}>
+            <ListItem.Subtitle>
+              {formatters.percentage(price_change_percentage_24h)}
+            </ListItem.Subtitle>
+          </View>
+          <View style={{ width: "30%", alignItems: 'flex-end' }}>
+            <ListItem.Title>
+              {formatters.priceUsd(current_price)}
+            </ListItem.Title>
+          </View>
+          {/* <ListItem.Title>{formatters.percentage(price_change_percentage_24h)}</ListItem.Title> */}
+        </View>
+      </ListItem.Content>
+    </ListItem>
+  );
+};
